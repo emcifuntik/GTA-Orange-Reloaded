a6d534605684ac240de28fd907fa322afdc588d1
Eugene Pogrebnyak <emcifuntik@gmail.com>
2016-12-18 00:04:31 +0300

    SetPauseMenu

--- orange-core/Scripts/GameInit.cpp
+++ orange-core/Scripts/GameInit.cpp
@@ -10,8 +10,7 @@ void Action()
 			SCRIPT::_REQUEST_STREAMED_SCRIPT(Utils::Hash("standard_global_init"));
 			CGlobals::Get().InitializeOnline();
 			ENTITY::SET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), 0.f, 0.f, 73.f, true, false, false, false);
-			CGlobals::Get().currentcam = 
-				CAM::CREATE_CAM("DEFAULT_SCRIPTED_CAMERA", false);
+			CGlobals::Get().currentcam = CAM::CREATE_CAM("DEFAULT_SCRIPTED_CAMERA", false);
 			CAM::SET_CAM_COORD(CGlobals::Get().currentcam, 0.f, 0.f, 100.f);
 			CAM::POINT_CAM_AT_COORD(CGlobals::Get().currentcam, 300.f, 300.f, 50.f);
 			CAM::SET_CAM_ACTIVE(CGlobals::Get().currentcam, true);
@@ -20,9 +19,8 @@ void Action()
 			UI::DISPLAY_RADAR(false);
 			CGlobals::Get().displayServerBrowser = true;
 			teleported = true;
-			CChat::Get()->AddChatMessage("Grand Theft Auto: Orange loaded")
+			CChat::Get()->AddChatMessage("Grand Theft Auto: Orange loaded");
 		}
-		UI::SET_PAUSE_MENU_ACTIVE(false);
 		scriptWait(0);
 	}
 }
