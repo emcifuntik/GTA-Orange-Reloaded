3788c087696f135c6961b62661a47211f66bb510
Eugene Pogrebnyak <emcifuntik@gmail.com>
2017-01-21 23:00:01 +0300

    Call stack in script->Run

--- orange-core/Core/scrManager.cpp
+++ orange-core/Core/scrManager.cpp
@@ -1,6 +1,45 @@
 #include "stdafx.h"
 
+#include "DbgHelp.h"
+#include <WinBase.h>
+
 #pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "Dbghelp.lib")
+
+void printStack(std::fstream& out)
+{
+	typedef USHORT(WINAPI *CaptureStackBackTraceType)(__in ULONG, __in ULONG, __out PVOID*, __out_opt PULONG);
+	CaptureStackBackTraceType func = (CaptureStackBackTraceType)(GetProcAddress(LoadLibrary(L"kernel32.dll"), "RtlCaptureStackBackTrace"));
+
+	if (func == NULL)
+		return; // WOE 29.SEP.2010
+
+				// Quote from Microsoft Documentation:
+				// ## Windows Server 2003 and Windows XP:  
+				// ## The sum of the FramesToSkip and FramesToCapture parameters must be less than 63.
+	const int kMaxCallers = 62;
+
+	void         * callers_stack[kMaxCallers];
+	unsigned short frames;
+	SYMBOL_INFO  * symbol;
+	HANDLE         process;
+	process = GetCurrentProcess();
+	SymInitialize(process, NULL, TRUE);
+	frames = (func)(0, kMaxCallers, callers_stack, NULL);
+	symbol = (SYMBOL_INFO *)calloc(sizeof(SYMBOL_INFO) + 256 * sizeof(char), 1);
+	symbol->MaxNameLen = 255;
+	symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+
+	const unsigned short  MAX_CALLERS_SHOWN = 6;
+	frames = frames < MAX_CALLERS_SHOWN ? frames : MAX_CALLERS_SHOWN;
+	for (unsigned int i = 0; i < frames; i++)
+	{
+		SymFromAddr(process, (DWORD64)(callers_stack[i]), 0, symbol);
+		out << "*** " << i << ": " << callers_stack[i] << " " << symbol->Name << " - 0x" << symbol->Address << std::endl;
+	}
+
+	free(symbol);
+}
 
 ScriptManagerThread g_ScriptManagerThread;
 
@@ -25,11 +64,14 @@ void Script::Tick()
 	else
 	{
 		scriptFiber = CreateFiber(NULL, [](LPVOID handler) {
-			__try {
+			try {
 				reinterpret_cast<Script*>(handler)->Run();
 			}
-			__except (EXCEPTION_EXECUTE_HANDLER) {
-				log_error << "Error in script->Run" << std::endl;
+			catch (...) {
+				std::string fname = DateTimeA() + ".call_stack.txt";
+				std::fstream callStackFile(fname, std::iostream::out);
+				printStack(callStackFile);
+				log_error << "Error in script->Run. Callstack was written to " << fname << std::endl;
 			}
 		}, this);
 	}
