bffb88972842ee2fb8613d509518b1b7c1a6c232
Eugene Pogrebnyak <emcifuntik@gmail.com>
2017-01-21 17:58:10 +0300

    Автодополнение

--- orange-core/Core/Chat.cpp
+++ orange-core/Core/Chat.cpp
@@ -146,6 +146,67 @@ void CChat::Render()
 		auto callback = [](ImGuiTextEditCallbackData* data) {
 			switch (data->EventFlag)
 			{
+				case ImGuiInputTextFlags_CallbackCompletion:
+				{
+					// Example of TEXT COMPLETION
+
+					// Locate beginning of current word
+					const char* word_end = data->Buf + data->CursorPos;
+					const char* word_start = word_end;
+					while (word_start > data->Buf)
+					{
+						const char c = word_start[-1];
+						if (c == ' ' || c == '\t' || c == ',' || c == ';')
+							break;
+						word_start--;
+					}
+
+					// Build a list of candidates
+					ImVector<const char*> candidates;
+					auto players = CNetworkPlayer::All();
+					for (int i = 0; i < players.size(); i++)
+						if (_strnicmp(players[i]->GetName().c_str(), word_start, (int)(word_end - word_start)) == 0)
+							candidates.push_back(players[i]->GetName().c_str());
+					if (_strnicmp(CConfig::Get()->sNickName.c_str(), word_start, (int)(word_end - word_start)) == 0)
+						candidates.push_back(CConfig::Get()->sNickName.c_str());
+
+					if (candidates.Size == 0)
+					{
+					}
+					else if (candidates.Size == 1)
+					{
+						// Single match. Delete the beginning of the word and replace it entirely so we've got nice casing
+						data->DeleteChars((int)(word_start - data->Buf), (int)(word_end - word_start));
+						data->InsertChars(data->CursorPos, candidates[0]);
+						data->InsertChars(data->CursorPos, " ");
+					}
+					else
+					{
+						// Multiple matches. Complete as much as we can, so inputing "C" will complete to "CL" and display "CLEAR" and "CLASSIFY"
+						int match_len = (int)(word_end - word_start);
+						for (;;)
+						{
+							int c = 0;
+							bool all_candidates_matches = true;
+							for (int i = 0; i < candidates.Size && all_candidates_matches; i++)
+								if (i == 0)
+									c = toupper(candidates[i][match_len]);
+								else if (c == 0 || c != toupper(candidates[i][match_len]))
+									all_candidates_matches = false;
+							if (!all_candidates_matches)
+								break;
+							match_len++;
+						}
+
+						if (match_len > 0)
+						{
+							data->DeleteChars((int)(word_start - data->Buf), (int)(word_end - word_start));
+							data->InsertChars(data->CursorPos, candidates[0], candidates[0] + match_len);
+						}
+					}
+
+					break;
+				}
 				case ImGuiInputTextFlags_CallbackHistory:
 				{
 					const int prev_history_pos = HistoryPos;
@@ -173,7 +234,7 @@ void CChat::Render()
 			}
 			return 0;
 		};
-		if (ImGui::InputText("", CGlobals::Get().chatBuffer, 256, ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_CallbackHistory, callback))
+		if (ImGui::InputText("", CGlobals::Get().chatBuffer, 256, ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_CallbackHistory | ImGuiInputTextFlags_CallbackCompletion, callback))
 		{
 			if (strlen(CGlobals::Get().chatBuffer))
 			{
