23acd8f058775fb4e4fc8728234267e246701c62
Marek Kraus <gamelaster@outlook.com>
2017-05-21 22:43:57 +0200

    Changes in scripts

--- nodejs-module/nodejs-module/orangeAPI/Player.js
+++ nodejs-module/nodejs-module/orangeAPI/Player.js
@@ -6,39 +6,7 @@ class Player {
     this.playerId = parseInt(playerId);
   }
 
-  get position() {
-    var pos = orange.getPlayerPosition(this.playerId);
-    var playerId = this.playerId;
-    var obj = {
-      get x() {
-        return pos[0];
-      },
-      get y() {
-        return pos[1];
-      },
-      get z() {
-        return pos[2];
-      },
-
-      set x(value) {
-        pos[0] = value;
-        orange.setPlayerPosition(playerId, pos[0], pos[1], pos[2]);
-      },
-      set y(value) {
-        pos[1] = value;
-        orange.setPlayerPosition(playerId, pos[0], pos[1], pos[2]);
-      },
-      set z(value) {
-        pos[2] = value;
-        orange.setPlayerPosition(playerId, pos[0], pos[1], pos[2]);
-      }
-    }
-    return obj;
-  }
-
-  set position(pos) {
-    orange.setPlayerPosition(this.playerId, pos.x, pos.y, pos.z);
-  }
+  
 }
 
 module.exports = Player;
